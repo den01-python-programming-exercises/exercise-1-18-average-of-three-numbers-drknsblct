9a84d69000731813002feb1b0c0cfbe878cb93d9
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 02:16:19 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    num1 = int(input('Give the first number:'))
+    num2 = int(input('Give the second number:'))
+    num3 = int(input('Give the second number:'))
+    
+                     
+    print('The average is ' + str((num1+num2+num3)/3))
 
 if __name__ == '__main__':
     main()
