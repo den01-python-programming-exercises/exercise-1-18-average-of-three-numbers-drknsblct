c28c5ac00fc0622bc73aa6e7ff2ccfe9447c5481
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 02:21:05 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -2,7 +2,7 @@ def main():
     #write your code below this line
     num1 = int(input('Give the first number:'))
     num2 = int(input('Give the second number:'))
-    num3 = int(input('Give the second number:'))
+    num3 = int(input('Give the third number:'))
     
                      
     print('The average is ' + str((num1+num2+num3)/3))
